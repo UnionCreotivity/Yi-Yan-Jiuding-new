38ba0cb435bf2d14004789a358bf3f3333409273
jshan <jshan0309@hotmail.com>
2024-11-07 11:57:24 +0800

    feat. 轉成石云建設

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,254 @@
 window.onload = function () {
-    // gsap.to('.outer', {
-    //     clipPath: 'polygon(6% 33%, 94% 33%, 94% 67%, 6% 67%)',
-    //     clipPath: 'polygon(0% -13%, 100% 87%, 100% 121%, 0% 17%)',
-    //     scrollTrigger: {
-    //         trigger: '.outer',
-    //         start: 'top top',
-    //         end: '+=100%',
-    //         scrub: true,
-    //         pin: true
-    //     }
-    // })
+
     var window_width = window.screen.width;
     var window_height = window.innerHeight;
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-    // clipPath: 'polygon(27% 12%, 73% 12%, 73% 88%, 27% 88%)',
-    //     clipPath: 'polygon(15% 25%, 85% 25%, 85% 75%, 15% 75%)',
-    //clipPath: 'polygon(6% 33%, 94% 33%, 94% 67%, 6% 67%)',
-    // clipPath: 'polygon(0% -13%, 100% 87%, 100% 121%, 0% 17%)',//斜切
+
+    function videoChange() {
+        const videoSources = [
+            "../assets/images/BG_black_1.mp4",
+        ];
+
+        const randomIndex = Math.floor(Math.random() * videoSources.length);
+        const selectedVideo = videoSources[randomIndex];
+
+        const videoElement = document.getElementById('video');
+        videoElement.src = selectedVideo;
+
+    }
+    videoChange();
 
 
-    let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.outer',
-            start: 'top top',
-            end: '+=100%',
-            scrub: true,
-            pin: true
-        }
-    });
+    function c1Ani() {
+        let title1 = gsap.utils.toArray(".index-box .content-box .title-1");
+        let splitEN1 = title1.map(heading => new SplitText(heading, {
+            type: "chars,words,lines", linesClass: "clip-text"
+        }));
+        let title2 = gsap.utils.toArray(".index-box .content-box .title-2");
+        let splitEN2 = title2.map(heading => new SplitText(heading, {
+            type: "chars,words,lines", linesClass: "clip-text"
+        }));
 
-    tl.to('.outer', {
+        let titleZH = gsap.utils.toArray(".index-box .content-box .zh-title");
+        let splitZH = titleZH.map(heading => new SplitText(heading, {
+            type: "chars,words,lines", linesClass: "clip-text"
+        }));
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.content-box',
+                start: 'top top',
+                end: '+=150%',
+                scrub: 2,
+                pin: true,
+                pinSpacing: false,
+            }
+        });
 
-        clipPath: 'polygon(6% 33%, 94% 33%, 94% 67%, 6% 67%)',//寬長方形
+        tl.to('.outer', {
+            clipPath: 'polygon(11% 33%, 89% 33%, 89% 67%, 11% 67%)',
+        }).to('.title-1-box', {
+            duration: 1,
+            ease: 'power2.inOut',
+            left: '33.5%',
+            top: '27.5%',
+            stagger: {
+                each: 0.05,
+                from: 'end'
+            }
+        }, '<')
+            .to('.title-svg-box', {
+                duration: 1,
+                ease: 'power2.inOut',
+                left: '33.7%',
+                top: '26.5%',
+                stagger: {
+                    each: 0.05,
+                    from: 'end'
+                }
+            }, '<')
+            .to('.title-2-box', {
+                duration: 0.7,
+                ease: 'power2.inOut',
+                left: '28%',
+                top: '60.8%',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            }, '<')
+            .to('.zh-title', {
+                duration: 0.7,
+                ease: 'power2.inOut',
+                top: '48.5%',
+            }, '<')
+            .to('.outer', {
+                clipPath: 'polygon(-20% -20%, 120% 107%, 120% 120%, -20% -7%)',
+            })
+            .to(splitEN1[0].chars, {
+                duration: 1,
+                ease: 'power2.inOut',
+                y: 140,
+                stagger: {
+                    each: 0.07,
+                    from: 'end'
+                }
+            }, '<-0.1')
+            .to('.title1-svg', { duration: 1, y: '3vw', ease: 'power2.inOut', }, '<0.2')
+            .to(splitEN2[0].chars, {
+                duration: 1,
+                ease: 'power2.inOut',
+                y: 300,
+                stagger: {
+                    each: 0.07,
+                    from: 'end'
+                }
+            }, '<-0.1')
+            .to(splitZH[0].chars, {
+                duration: 1,
+                ease: 'power2.inOut',
+                y: 300,
+                stagger: {
+                    each: 0.07,
+                    from: 'end'
+                }
+            }, '<')
+            .to('.outer', {
+                clipPath: 'polygon(50% 0%, 50% 100%, 50% 100%, 50% 0%)', //直線
+            }, '<0.6')
+            .to('.c1-logo', {
+                opacity: 0,
+                ease: 'power2.inOut',
+            }, '<0.3')
 
-    }).to('.outer', {
-        clipPath: 'polygon(0% -13%, 100% 87%, 100% 121%, 0% 17%)', //斜切
-    })
+    }
+    c1Ani();
 
+    function c2Ani() {
+
+        let smallENtitleArray = gsap.utils.toArray(".black-text");
+        let smallENtitle = smallENtitleArray.map(heading => new SplitText(heading, {
+            type: "chars,words,lines", linesClass: "clip-text"
+        }));
+
+        let smallENtitle2Array = gsap.utils.toArray(".black2-text");
+        let smallENtitle2 = smallENtitle2Array.map(heading => new SplitText(heading, {
+            type: "chars,words,lines", linesClass: "clip-text"
+        }));
+
+        let smallENtitleGoldArray = gsap.utils.toArray(".gold-text");
+        let smallENtitle3 = smallENtitleGoldArray.map(heading => new SplitText(heading, {
+            type: "chars,words,lines", linesClass: "clip-text"
+        }));
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.c2-text-content-box',
+                // start: 'top top',
+                // end: '+=100%',
+                start: 'top top',
+                end: '+=400%',
+                scrub: 3,
+                pin: true,
+                pinSpacing: true,
+            }
+        });
+        tl
+            .to('.black1 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            })
+
+            .to('.black2 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            }, '<')
+
+            .to('.black3 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            }, '<')
+
+
+            .to('.gold1 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            })
+            .to('.gold2 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            }, '<')
+            .to('.gold3 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            }, '<')
+
+            .to('.black2-1 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            })
+            .to('.black2-2 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            }, '<')
+            .to('.black2-3 .clip-text div div', {
+                clipPath: 'inset(0% 0% 0% 0%)',
+                stagger: {
+                    each: 0.05,
+                    from: 'start'
+                }
+            }, '<')
+
+            .to('.red-logo', { duration: 1, opacity: 1, }, '<0.8')
+            .to('.red-logo', { duration: 1, opacity: 0, }, '<0.15')
+            .to('.c2-img,.right-text-svg,.top-small-text-box', { duration: 1, opacity: 1, ease: 'power1.inOut', }, '<0.2')
+            .to('.c2-video', { duration: 1, maskSize: '7%', ease: 'power1.inOut' }, '<0.3')
+            .fromTo('.c2-video', { maskPosition: '53%' },
+                { duration: 5, maskSize: '1300%', maskPosition: '45%' })
+            .to('.black-box,.right-text-svg,.top-small-text-box', { duration: 2, y: '-43vw' }, '<0.3')
+            .fromTo('.card3', { y: '10vw' }, { y: 0, duration: 3, opacity: 1, ease: 'power1.inOut', }, '<-0.25')
+
+
+    }
+    c2Ani();
+
+    function c3Ani() {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.main-content-box',
+                start: '-80%',
+                end: '+=100%',
+                scrub: 2,
+
+            }
+        });
+
+        tl.to('.c2-text-content-box', { duration: 1, scale: '0.85' })
+    }
+    // c3Ani();
 }
+
